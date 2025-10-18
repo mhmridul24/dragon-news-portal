eff4f28b0501c86f32af95830868987e9a3af2f3
mhmridul24 <yourmridul24@gmail.com>
2025-10-18 10:51:21 +0600

    fixed the theme color as per design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import logo from "../assets/logo.png"
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
-            <img src={logo} alt="Logo" />
-            <p>Journalism Without Fear or Favour</p>
+            <img className='mt-5' src={logo} alt="Logo" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
         </div>
     );
 };
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -14,6 +14,7 @@ const HomeLayout = () => {
                     <Outlet></Outlet>
                 </section>
                 <section className="right_nav"></section>
+                
             </main>
         </div>
     );
