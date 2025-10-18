ced8ef52bef52f81bc0f62066fc0d3ac135afd54
mhmridul24 <yourmridul24@gmail.com>
2025-10-18 13:02:11 +0600

    Added the headline news section with marquee

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const LatestNews = () => {
+    return (
+        <div className='flex items-center gap-5 bg-base-200 p-3'>
+            <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
+            <Marquee className='flex gap-5' pauseOnHover={true}speed={60}>
+                <p className='font-bold'>Groundwork laid for sweeping constitutional reforms</p>
+                <p className='font-bold'>Robbery attempt foiled on Dhaka-Sylhet old highway</p>
+            </Marquee>
+        </div>
+    );
+};
+
+export default LatestNews;
