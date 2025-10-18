9072f012866e69b36c3c30a16f23bc10dcd28e2a
mhmridul24 <yourmridul24@gmail.com>
2025-10-18 12:19:10 +0600

    added dynamic date benith of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
             <img className='mt-5' src={logo} alt="Logo" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-accent font-semibold'> 
+                <span className='text-black'>{format(new Date(), 'EEEE,')}</span> <span>{format(new Date(), 'MMMM dd, yyyy')}</span>
+            </p>
         </div>
     );
 };
