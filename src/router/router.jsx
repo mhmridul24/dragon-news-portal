64f84c71d487658d9bb4af4532cb77bb196c186d
mhmridul24 <yourmridul24@gmail.com>
2025-10-18 10:37:29 +0600

    added the header section

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layout/HomeLayout";
 
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element: <h2>Home Layout</h2>
+            element: <HomeLayout></HomeLayout>,
         },
         {
             path: "/auth",
-            element: <h2>Authentication Layout</h2>
+            element: <h2>Authentication Layout</h2>,
         },
         {
             path: "/news",
-            element: <h2>News Layout</h2>
+            element: <h2>News Layout</h2>,
         },
         {
             path: "/*",
-            element: <h2>Error404</h2>
+            element: <h2>Error404</h2>,
         }
     ]
 );
